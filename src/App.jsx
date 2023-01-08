afd99258b5a17009a5f635bf1e6149b33c418d2f
Alex <ezhil2744@gmail.com>
2023-01-08 21:41:49 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import img from '../public/man.jpg'
+
+const App = () => {
+  return (
+    <div className="container">
+     hi
+    </div>
+  );
+}
+
+export default App;
